48580575a33ff5783a88e6ef6596d5c70705af55
Modder King <127608272+shakir04@users.noreply.github.com>
2024-01-10 20:33:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
-# Don't Remove Credit @movie_file_20
-# Subscribe YouTube Channel For Amazing Bot @movie_file_20
-# Ask Doubt on telegram @KingVJ01
+
 
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ELINA-BOTS/Neha.git /Neha
+  git clone https://github.com/shakir04/Neha.git /Neha
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Neha
