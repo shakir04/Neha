eb83981de93e7132118134476dca49a183f7a340
Modder King <127608272+shakir04@users.noreply.github.com>
2024-01-10 21:12:27 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(Kashmira, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powed by :</b> @IM_JISSHU
+<b>Powed by :</b> @mr_shakir_04
 
 """
         if qw.get("description"):
