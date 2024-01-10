c1394f2bbbd076edd7bfeb2a6453f6551a1f0dc8
Modder King <127608272+shakir04@users.noreply.github.com>
2024-01-10 21:11:49 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -19,5 +19,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('JISSHU_BOTS', url='https://t.me/JISSHU_BOTS')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('ｋｉｎｇ', url='https://t.me/mr_shakir_04')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
