a4002adfc29333616e0262cb9538cb5a3833cbba
Modder King <127608272+shakir04@users.noreply.github.com>
2024-01-16 17:45:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-
-
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, IS_SHORTLINK, LOG_CHANNEL, TUTORIAL, GRP_LNK, CHNL_LNK, CUSTOM_FILE_CAPTION
