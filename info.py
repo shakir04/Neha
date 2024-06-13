f2585c5d6622c22d385bb30fb16053468ec5bf00
Modder King <127608272+shakir04@users.noreply.github.com>
2024-06-13 19:10:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,8 +97,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://mrshakir04-modderking1-e89d2cbd.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://mrshakir04-modderking1-e89d2cbd.koyeb.app/".format(FQDN, PORT)
+URL = "https://movieszonereborn-modderking1-3a55a84e.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://movieszonereborn-modderking1-3a55a84e.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -113,9 +113,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://mrshakir04-modderking1-e89d2cbd.koyeb.app/".format(FQDN)
+    URL = "https://movieszonereborn-modderking1-3a55a84e.koyeb.app/".format(FQDN)
 else:
-    URL = "https://mrshakir04-modderking1-e89d2cbd.koyeb.app/".format(FQDN)
+    URL = "https://movieszonereborn-modderking1-3a55a84e.koyeb.app/".format(FQDN)
 
 
 
